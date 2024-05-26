b0d9f7d7a53d7cfb0adeef829671b552c57308ab
roma <roma.okhrimenko@gmail.com>
2024-05-26 11:38:01 +0300

    feat: set up axios and useHttp hook

---
+++ src/@core/composables/useHttp.ts
@@ -0,0 +1,66 @@
+import axios from '@core/axios/axios.ts'
+import { ref } from 'vue'
+
+const DEFAULT_OPTIONS = {
+  method: 'GET',
+  showErrorToast: true,
+  params: {},
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms))
+
+export const useHttp = (options) => {
+  let opts = {
+    ...DEFAULT_OPTIONS,
+    ...options,
+  }
+
+  let data = ref(null)
+  let error = ref(null)
+  let isLoading = ref(false)
+  let isReady = ref(false)
+
+  const onError = (err) => {
+    console.error(err)
+    error.value = err
+    opts.showErrorToast && alert(handledError)
+
+    isReady.value = false
+    isLoading.value = false
+
+    throw err
+  }
+  const onSuccess = (response) => {
+    data.value = response.data
+
+    isLoading.value = false
+    isReady.value = true
+  }
+
+  const execute = async (
+    { params = {}, url = null, payload = {} },
+    successCallback = () => {},
+    errorCallback = () => {}
+  ) => {
+    try {
+      isLoading.value = true
+
+      // await delay(1000)
+
+      const response = await axios({
+        method: opts.method,
+        params: params ?? {},
+        url,
+        data: payload,
+      })
+
+      onSuccess(response)
+      successCallback(data.value)
+    } catch (error) {
+      errorCallback(error)
+      onError(error)
+    }
+  }
+
+  return { data, isLoading, isReady, execute, error }
+}
